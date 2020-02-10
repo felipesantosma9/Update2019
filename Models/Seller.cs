b6abec089cdeb6fd90e4b15e8329d227be24251d
LuizFelipeMa9 <luiz.santos@ma9.com.br>
2020-02-10 14:40:02 -0300

    Validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,15 +9,22 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ser entre {2} e {1}")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Entre com um e-mail válido")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         public string Email { get; set; }
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         public DateTime BirthDate { get; set; }
+        [Range(100.0, 50000.0, ErrorMessage = "{0} no mínimo de {1} até {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }
         public int DepartamentId { get; set; }
